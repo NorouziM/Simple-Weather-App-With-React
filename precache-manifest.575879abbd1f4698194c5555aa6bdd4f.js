6d06b561e036d15cf599c3919f07ba148a2f52da
NorouziM <mnorouzidev@gmail.com>
2020-10-17 23:21:30 +0330

    Updates

---
+++ precache-manifest.575879abbd1f4698194c5555aa6bdd4f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83424d0f1ef4c9b9b7d11cc87a166f59",
+    "url": "/Simple-Weather-App-With-React/index.html"
+  },
+  {
+    "revision": "094c576381c600f99e08",
+    "url": "/Simple-Weather-App-With-React/static/css/main.892a48ca.chunk.css"
+  },
+  {
+    "revision": "de04626190e8445775f5",
+    "url": "/Simple-Weather-App-With-React/static/js/2.0c9235bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Simple-Weather-App-With-React/static/js/2.0c9235bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "094c576381c600f99e08",
+    "url": "/Simple-Weather-App-With-React/static/js/main.a88e3171.chunk.js"
+  },
+  {
+    "revision": "5d34e7692e605c7c82b2",
+    "url": "/Simple-Weather-App-With-React/static/js/runtime-main.380915c4.js"
+  },
+  {
+    "revision": "ff63621298e5ec626dc791df574462d7",
+    "url": "/Simple-Weather-App-With-React/static/media/clear-day-cold-bg.ff636212.jpg"
+  },
+  {
+    "revision": "cc3eb103589afcf63b027db164bbfccc",
+    "url": "/Simple-Weather-App-With-React/static/media/clear-day-warm-bg.cc3eb103.jpg"
+  },
+  {
+    "revision": "073d81eab49695c5441c23bf7caf826d",
+    "url": "/Simple-Weather-App-With-React/static/media/clear-night-cold-bg.073d81ea.jpg"
+  },
+  {
+    "revision": "4ed861d2abe3a931f3a193873e6706a9",
+    "url": "/Simple-Weather-App-With-React/static/media/clear-night-warm-bg.4ed861d2.jpg"
+  }
+]);
